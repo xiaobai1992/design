b314460fcd2f2e1398db8586c1b00062287b903d
xiaobai <329976979@qq.com>
2018-11-28 14:47:58 +0800

    ‘d
    第一次上传

---
+++ src/index.js
@@ -0,0 +1,88 @@
+//继承
+// class Person {
+//     constructor(name,age){
+//         this.name = name
+//         this.age =age
+//     }
+//     eat(){
+//         alert(`${this.name} eat something`)
+//     }
+//     speak(){
+//         alert(`我的名字是${this.name}，我的年龄是${this.age}`)
+//     }
+// }
+// var zhang = new Person('zhang',20)
+// // alert(zhang.eat())
+// // alert(zhang.speak())
+//
+// class Student extends Person {
+//     constructor(name,age,number){
+//       super(name,age)
+//       this,number = number
+//   }
+//   study(){
+//       alert(`${this.name}正在学习`)
+//   }
+// }
+// var xiaoming = new Student('小米',20,1311)
+// xiaoming.eat()
+// xiaoming.study()
+
+
+
+
+//封装 ts  http://www.typescriptlang.org/play/index.html
+
+// class Person {
+//     name
+//     age
+//     protected weight
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//         this.weight = 120
+//     }
+//     eat(){
+//         alert(`${this.name} eat something`)
+//     }
+//     speak(){
+//         alert(`我的名字是${this.name}，我的年龄是${this.age}`)
+//     }
+//     getWeight() {
+//         alert(`我的体重是${this.weight}`)
+//     }
+// }
+//
+// class Student extends Person {
+//     number
+//     private gf
+//     constructor(name,age,number){
+//         super(name,age)
+//         this.number = number
+//         this.gf= 'xiaoli'
+//     }
+//     study(){
+//         alert(`${this.name}正在学习`)
+//     }
+//     getWeight() {
+//         alert(`我的体重是${this.weight}`)
+//     }
+// }
+// let xm = new Student('xm', 20,11111111)
+// xm.getWeight()
+// alert(xm.weight)
+
+//实例
+class jQuery {
+    constructor(selector){
+        let slice = Array.prototype.slice
+        let dom = slice.call(document.querySelectorAll(selector))
+        let len = dom?dom.length:0
+        for (let i = 0;i<len;i++){
+            this[i]=dom[i]
+        }
+        this.length = len
+        this.seletor = selector||''
+    }
+
+}
